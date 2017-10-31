f06934f89428d9f2237ddf4a6a7235b726daf873
lizhiming <liwiseap@163.com>
2017-10-31 13:36:04 +0800

    一秒现金系统

---
+++ oscash-service-timer/src/main/java/com/osc/oscashservicetimer/OscashServiceTimerApplication.java
@@ -0,0 +1,12 @@
+package com.osc.oscashservicetimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscashServiceTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscashServiceTimerApplication.class, args);
+	}
+}
