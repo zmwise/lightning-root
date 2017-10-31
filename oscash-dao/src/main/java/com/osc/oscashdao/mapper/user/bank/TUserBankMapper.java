998595b4d3e6ca25263af8dea0a21d1d0df5207e
lizhiming <liwiseap@163.com>
2017-10-31 13:54:48 +0800

    oscash-dao generator code

---
+++ oscash-dao/src/main/java/com/osc/oscashdao/mapper/user/bank/TUserBankMapper.java
@@ -0,0 +1,7 @@
+package com.osc.oscashdao.mapper.user.bank;
+
+import com.osc.oscashdao.entity.user.bank.TUserBank;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface TUserBankMapper extends Mapper<TUserBank> {
+}
